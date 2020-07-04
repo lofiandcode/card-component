e9c093748438a13ff7518159ca3de16e3ec547ab
Joseph Trettevik <josephtrettevik@gmail.com>
2020-07-04 10:47:18 -0700

    add card css with shadow

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
     return (
-        <div key={props.id}>
+        <div key={props.id} className="card">
         <h2>{props.name}</h2>
-        <h4>{props.price}</h4>
+        <h4>${props.price}</h4>
         <p>{props.descriptions}</p>
         </div>
     );
